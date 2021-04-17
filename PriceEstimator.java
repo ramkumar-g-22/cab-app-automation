0120055eb67118b868abd4e327802151b7727b0b
ramkumar-g-22 <82449964+ramkumar-g-22@users.noreply.github.com>
2021-04-17 14:27:59 +0530

    Add files via upload

--- PriceEstimator.java
+++ PriceEstimator.java
@@ -1,5 +1,7 @@
 package com.ramkumar;
 
+import java.time.LocalTime;
+
 public class PriceEstimator {
 
 	/**
@@ -11,6 +13,26 @@ public class PriceEstimator {
 	public static int calculateTotalFarePriceWithoutGST(int cabTypeFarePrice, int kilometer) {
 		int totalFarePrice = cabTypeFarePrice * kilometer;
 		return totalFarePrice;
+	}
+
+	/**
+	 * Calculates total fare price at peak time.
+	 * @param pickUpTime
+	 * @param totalFarePriceWithGST
+	 * @return
+	 */
+	public static double calculateTotalFareAtPeakTime(LocalTime pickUpTime, double totalFarePriceWithGST, int kilometer) {
+		int hour = pickUpTime.getHour();
+		double peakTimeFarePercentagePerKilometer = 1.25;
+		double farePriceAtPeakTime;
+		double totalFarePriceAtPeakTime;
+		if(hour >= 5 && hour <= 7) {
+			farePriceAtPeakTime = (totalFarePriceWithGST * (peakTimeFarePercentagePerKilometer * kilometer)) / 100.0;
+			totalFarePriceAtPeakTime = totalFarePriceWithGST + farePriceAtPeakTime;
+			return totalFarePriceAtPeakTime;
+		}else {
+			return totalFarePriceWithGST;			
+		}
 	}
 
 }
